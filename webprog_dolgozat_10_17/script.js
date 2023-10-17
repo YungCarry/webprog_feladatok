e0b45ff36254d10f0a224775fdc1f16169ee08a2
YungCarry <c9kerdom@vasvari.org>
2023-10-17 09:39:51 +0200

    kész

---
+++ webprog_dolgozat_10_17/script.js
@@ -0,0 +1,17 @@
+
+function javitas(){
+    var a = document.getElementByClassName("container");
+    a.style.bacgroundColor = "#12406A";
+    
+    var b = document.getElementByClassName("content1");
+    b.style.bacgroundColor = "#EB8F90";
+
+    var c = document.getElementByClassName("content2");
+    c.style.bacgroundColor = "#FFB471";
+
+    var d = document.getElementByClassName("content3");
+    d.style.bacgroundColor = "#ADBED2";
+
+    var e = document.getElementsByTagName("body")
+    e.style.bacgroundColor = "#CCFFFF";
+}

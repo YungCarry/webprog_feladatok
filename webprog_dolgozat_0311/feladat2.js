cac6df332b180a537f37e3abdfe1be66cda6f1ca
YungCarry <c9kerdom@vasvari.org>
2024-03-11 13:02:47 +0100

    kesz

---
+++ webprog_dolgozat_0311/feladat2.js
@@ -0,0 +1,15 @@
+function mineLocation(field) {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] === 1) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+//Tesztelés
+console.log("----- 2. feladat -----");
+console.log(mineLocation([ [1, 0], [0, 0] ]), [0, 0]);
+console.log(mineLocation([ [1, 0, 0], [0, 0, 0], [0, 0, 0] ]), [0, 0]);
+console.log(mineLocation([ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0] ]), [2, 2]);

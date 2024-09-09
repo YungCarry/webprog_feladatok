62982441af70b4b8e3dbc3b10a45d0693ee5300b
Yung Carry <c9kerdom@vasvari.org>
2024-09-09 09:18:06 +0200

    kesz

---
+++ CodeWars score/script.js
@@ -0,0 +1,18 @@
+async function getData() {
+    let username = document.getElementById(uname);
+    
+    const url = `https://www.codewars.com/api/v1/users/${username}`;
+    fetch(url)
+    .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    alert('A felhasználó pontja: '+data.ranks.overall.score)
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+  }

e12877029501417ec30a668191830e033b94dc6a
Yung Carry <c9kerdom@vasvari.org>
2024-09-16 09:40:08 +0200

    kesz

--- CodeWars score/script.js
+++ CodeWars score/script.js
@@ -1,27 +1,46 @@
 function getData() {
-    let username = document.getElementById(uname);
-    
-    const url = `https://www.codewars.com/api/v1/users/${username}`;
-    fetch(url)
-    .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    
-    
-    if (data.username === "null")
-    {
-      console.log("Helytelen Felhasználónév");
-    }
+  let username = document.getElementById("uname").value;
+  console.log(username);
 
-    else {
-      
-    }
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-  }
+  const url = `https://www.codewars.com/api/v1/users/${username}`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      let resultDiv = document.getElementById("result");
+      resultDiv.innerHTML = "";
+
+      if (!data.username) {
+        resultDiv.innerHTML = "<p>Helytelen Felhasználónév</p>";
+      } else {
+        if (document.getElementById("osszesitett").checked) {
+          resultDiv.innerHTML = `
+                <h2>Összesített: </h2>
+                <p>Pontszám: ${data.ranks.overall.score}</p>
+                <p>Honor: ${data.honor}</p>
+              `;
+        } else if (document.getElementById("nyelveksz").checked) {
+          const languages = data.ranks.languages;
+          let languageScores = "<h3>Nyelvek szerinti pontszám :</h3><ul>";
+          for (const [language, info] of Object.entries(languages)) {
+            languageScores += `<li>${language}: ${info.score}</li>`;
+          }
+          languageScores += "</ul>";
+          resultDiv.innerHTML = languageScores;
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+
+      let resultDiv = document.getElementById("result");
+      resultDiv.innerHTML =
+        "<p>Hiba történt az adatok lekérése során, kérjük próbálja később.</p>";
+    });
+}
